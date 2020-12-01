8af0753240db687244f502ee41f30ae84e0f195b
Iliyev Telman <telman.devops@gmail.com>
2020-12-01 17:42:15 +0600

    edit docker-compose file

--- src/comment/docker_build.sh
+++ src/comment/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER_NAME=${user_name:-870414}
+# USER_NAME=${user_name:-870414}
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
